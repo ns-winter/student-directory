29285ab085b156638e127056c19b2ad0012ba671
Nigel Said <nigelsaid@gmail.com>
2017-04-01 10:19:09 +0100

    adds Interactive Menu

--- directory.rb
+++ directory.rb
@@ -62,7 +62,7 @@ def print_footer(students)
   puts "----".center(line_width)
 end
 
-students = input_students
+
 
 def print_restricted(students)
   students.each_with_index do |student, index|
@@ -90,6 +90,8 @@ def print_by_cohort(students)
 end
 
 #nothing happens until we call the methods
+=begin
+students = input_students
 if students.count == 0
   puts
   puts "No students attend Villains Academy :("
@@ -99,3 +101,31 @@ else
   print_by_cohort(students)
   print_footer(students)
 end
+=end
+
+def interactive_menu
+  students = []
+  loop do
+    #1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    #2. read the input and save it into a variable
+    selection = gets.chomp
+    #3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print_standard(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+interactive_menu
